9b9fe4e8deebc26ac0d643ff89873ed14e8970de
Melvin Laplanche <melvin.wont.reply@gmail.com>
2019-04-14 00:23:31 -0700

    refactor: reorganize the whole code

--- main.go
+++ main.go
@@ -10,7 +10,7 @@ import (
 	flag "github.com/spf13/pflag"
 )
 
-// Module represent a single Go module
+// Module represents a single Go module
 type Module struct {
 	Path     string       // module path
 	Version  string       // module version
@@ -30,65 +30,79 @@ type ModuleError struct {
 	Err string
 }
 
+// Flags represents all the flags accepted by the CLI
+type Flags struct {
+	CheckOldPkgs   bool
+	CheckIndirects bool
+	IgnoredPkgs    []string
+}
+
 func main() {
-	checkOldPkgs := flag.Bool("check-old", false, "check for modules without updates for the last 6 months")
-	checkIndirect := flag.Bool("check-indirect", false, "check indirect modules")
-	ignoredPkgs := flag.StringSliceP("ignore", "i", []string{}, "coma separated list of packages to ignore")
+	flags := &Flags{}
+	flag.BoolVar(&flags.CheckOldPkgs, "check-old", false, "check for modules without updates for the last 6 months")
+	flag.BoolVar(&flags.CheckIndirects, "check-indirects", false, "check indirect modules")
+	flag.StringSliceVarP(&flags.IgnoredPkgs, "ignore", "i", []string{}, "coma separated list of packages to ignore")
 	flag.Parse()
 
+	// get an invalid JSON list of all modules
 	out, err := Run("go", "list", "-m", "-u", "-json", "all")
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	// make the output valid JSON
+	// make list a valid JSON list
 	out = "[" + out + "]"
 	out = strings.ReplaceAll(out, "}\n{", "},\n{")
 
+	// Parse the JSON list into or Go Slice
 	modules := []*Module{}
 	err = json.Unmarshal([]byte(out), &modules)
 	if err != nil {
 		log.Fatal(err)
 	}
 
+	// check every modules one-by-one
 	for _, m := range modules {
-		skip := false
-		for _, pkg := range *ignoredPkgs {
-			if strings.HasPrefix(m.Path, pkg) {
-				skip = true
-			}
-		}
-		if skip {
-			continue
-		}
+		checkModule(flags, m)
+	}
+}
 
-		tag := ""
-		if m.Indirect {
-			if !*checkIndirect {
-				continue
-			}
-			tag = "[indirect] "
+// checkModule checks a single module and prints its status
+func checkModule(f *Flags, m *Module) {
+	for _, pkg := range f.IgnoredPkgs {
+		if strings.HasPrefix(m.Path, pkg) {
+			return
 		}
+	}
 
-		// Report if the package has been replaced
-		if m.Replace != nil {
-			fmt.Printf(tag+"%s has been replaced by %s\n", m.Path, m.Replace.Path)
-			continue
-		}
+	if m.Indirect && !f.CheckIndirects {
+		return
+	}
 
-		// Report if the package has an update available
-		if m.Update != nil {
-			fmt.Printf(tag+"%s can be updated to %s\n", m.Path, m.Update.Version)
-			continue
-		}
+	// Set the tags
+	tag := ""
+	if m.Indirect {
+		tag = "[indirect] "
+	}
 
-		// Report if the package hasn't been updated in 6 months
-		if *checkOldPkgs && m.Time != nil {
-			sixMonths := 6 * 30 * 24 * time.Hour
-			if time.Since(*m.Time) >= sixMonths {
-				fmt.Printf(tag+"%s hasn't been updated in over 6 months (%s)\n", m.Path, m.Time.String())
-				continue
-			}
+	// Report if the package has been replaced
+	if m.Replace != nil {
+		fmt.Printf(tag+"%s has been replaced by %s\n", m.Path, m.Replace.Path)
+		return
+	}
+
+	// Report if the package has an update available
+	if m.Update != nil {
+		fmt.Printf(tag+"%s can be updated to %s\n", m.Path, m.Update.Version)
+		return
+	}
+
+	// Report if the package hasn't been updated in 6 months
+	if f.CheckOldPkgs && m.Time != nil {
+		sixMonths := 6 * 30 * 24 * time.Hour
+		if time.Since(*m.Time) >= sixMonths {
+			fmt.Printf(tag+"%s hasn't been updated in over 6 months (%s)\n", m.Path, m.Time.String())
+			return
 		}
 	}
 }
